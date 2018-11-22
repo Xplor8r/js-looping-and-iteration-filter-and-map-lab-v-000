16ab36af95b43bf730e59ba24c54d4b4a6e55964
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-22 19:17:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue).map( function(driver) { return driver.name; });
 }
 
-function exactMatch() {
-
+function exactMatch(drivers, attribute) {
+  return drivers.filter( function(driver) { 
+    let exact;
+    return driver.revenue = revenue;});
+    for (const key in attribute) {
+      exact = driver[key] === attribute[key]
+    }
 }
 
 function exactMatchToList() {
