8ac74735b5a8d1ec2c5685a8a0fe48b32656d43f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-22 19:10:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution here:
 function driversWithRevenueOver (drivers, revenue) {
-  return drivers.filter(function(driver) { return driver.revenue > revenue;});
+  return drivers.filter( function(driver) { return driver.revenue > revenue;});
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue).map( function (driver) { return driver.name; });
+  return driversWithRevenueOver(drivers, revenue).map( function(driver) { return driver.name; });
 }
 
 function exactMatch() {
